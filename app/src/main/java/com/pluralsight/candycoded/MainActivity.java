9482c50fd21d4fcfa212e538284a64c9e6d1198e
ol4 juwon <olajuwonlawal@icloud.com>
2020-09-24 19:19:03 +0100

    Completed Module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -29,14 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private Candy[] candies;
     private CandyDbHelper candyDbHelper = new CandyDbHelper(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
 
-        Intent infoIntent = new Intent(this, InfoActivity.class);
-        startActivity(infoIntent);
-        return super.onOptionsItemSelected(item);
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,6 +85,15 @@ public class MainActivity extends AppCompatActivity {
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent infoIntent = new Intent(this, InfoActivity.class);
+        startActivity(infoIntent);
+        return super.onOptionsItemSelected(item);
+
+    }
+
 
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
